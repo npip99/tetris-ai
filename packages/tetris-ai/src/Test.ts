25fceeb5b1b07fd2ba8b8f9d67661c12d94688c0
Nicholas Pipitone <npip99@gmail.com>
2022-02-25 01:49:36 -0500

    IMPERATIVE, TMP

--- packages/tetris-ai/src/Test.ts
+++ packages/tetris-ai/src/Test.ts
@@ -20,37 +20,69 @@ setTimeout(async () => {
 
     let nnBatcher = new NNBatcher(await fallingStarNN.getNNModel(), BATCH_SIZE, 1.0);
 
-    let numEvaluations = 0;
-    let getNNResult = async (inputTensor: tf.Tensor3D): Promise<number[][]> => {
-        // Await on the calculation to finish
-        numEvaluations++;
-        let result = await nnBatcher.getNNResult(inputTensor);
-        return result;
-    };
-
     let startTime = performance.now();
 
-    let MCTSPromises: Promise<void>[] = [];
+    let numEvaluations = 0;
+    let getNNResult = (inputTensor: tf.Tensor3D): number[][] => {
+        numEvaluations++;
+        if (numEvaluations % 5000 == 0) {
+            console.log("Avg Eval %dms (%d Evaluations)", (performance.now() - startTime) / numEvaluations, numEvaluations);
+        }
+        // Await on the calculation to finish
+        const USING_NN = false;
+        if (USING_NN) {
+            //let result = await nnBatcher.getNNResult(inputTensor);
+            return [];
+        } else {
+            return [
+                [1/2],
+                [1/3, 1/3, 1/3],
+            ];
+        }
+    };
+
+    let simultaneousMCTSs: MCTS[] = [];
     for(let i = 0; i < NUM_SIMULTANEOUS_MCTS; i++) {
-        MCTSPromises.push((async () => {
-            let fallingStarInitState = fallingStarGame.getInitialState();
+        let fallingStarInitState = fallingStarGame.getInitialState();
+        simultaneousMCTSs.push(new MCTS(fallingStarGame, fallingStarInitState, {
+            numMCTSSims: 100,
+            numParallelSims: MCTS_BATCH_SIZE,
+            gamma: 0.99,
+            // Training, 1 for 500k training steps, 0.5 for 250k training steps, 0.25 for 250k training steps
+            temperature: 1.0,
+        }));
+    }
 
-            // Run a MCTS from the initial state
-            let fallingStarMCTS = new MCTS(fallingStarGame, fallingStarInitState, getNNResult, {
-                numMCTSSims: 100,
-                numParallelSims: MCTS_BATCH_SIZE,
-                gamma: 0.99,
-                // Training, 1 for 500k training steps, 0.5 for 250k training steps, 0.25 for 250k training steps
-                temperature: 1.0,
-            });
-
-            // Run 10 steps of the simulation
-            for(let step = 0; step < 3 && !fallingStarMCTS.isGameOver(); step++) {
-                await fallingStarMCTS.iterate();
-                // console.log("Step: %d", step);
-                // fallingStarMCTS.print();
-                fallingStarMCTS.sampleMove();
+    while(simultaneousMCTSs.filter(mcts => mcts != null).length > 0) {
+        for(let i = 0; i < simultaneousMCTSs.length; i++) {
+            let mcts = simultaneousMCTSs[i];
+            if (mcts == null) {
+                continue;
             }
+
+            // Get the tensors that the MCTS wants
+            let desiredTensors = mcts.iterate();
+            // If the MCTS isn't desiring any more simulations, it's time to sample a move first
+            if (desiredTensors.length == 0) {
+                mcts.sampleMove();
+                // If the game is over now,
+                // null it from the list and continue
+                if (mcts.isGameOver()) {
+                    simultaneousMCTSs[i] = null;
+                    continue;
+                }
+                desiredTensors = mcts.iterate();
+            }
+
+            // Get the NN results
+            let resultTensors = [];
+            for(let i = 0; i < desiredTensors.length; i++) {
+                resultTensors.push(getNNResult(desiredTensors[i]));
+            }
+
+            // Commit them to the MCTS
+            mcts.commitNNResults(resultTensors);
+
             // console.log("Done!");
             // fallingStarMCTS.print();
             // console.log(fallingStarMCTS.trainingData.length);
@@ -67,11 +99,8 @@ setTimeout(async () => {
                     ],
                 );
             }*/
-        })());
+        }
     }
-
-    // Wait for them all to finish
-    await Promise.all(MCTSPromises);
 
     let totalTime = (performance.now() - startTime);
     console.log("Avg Evaluation Time: %dms (%d Evaluations)", (totalTime/numEvaluations).toPrecision(3), numEvaluations);
